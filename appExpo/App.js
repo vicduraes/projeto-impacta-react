4035db629e2491b7d74a981ee9aac123d03a24cc
vicduraes <duraes.vic@gmail.com>
2019-11-09 17:37:45 -0300

    Change color

--- appExpo/App.js
+++ appExpo/App.js
@@ -1,6 +1,16 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native'
 import axios from 'axios'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Dimensions
+} from 'react-native';
+import Axios from 'axios';
+
+const width = Dimensions.get('window').width
 
 const URL = "http://172.16.20.59:3200/api/curso"
 
@@ -11,18 +21,24 @@ export default class App extends React.Component {
     isRefreshing: false
   }
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = this.initialState
   }
 
   componentDidMount() {
+      this._getLista()
+  }
+
+  _getLista() {
+    this.setState({...this.state, isRefreshing : true})
     axios.get(URL).then(response => {
-      if(response.data) {
-        this.setState({...this.state, data: response.data})
+      if (response.data) {
+        this.setState({ ...this.state, data: response.data, isRefreshing : false})
       }
     }).catch(error => {
       console.log(error)
+      this.setState({...this.state, isRefreshing : false})
     })
   }
 
@@ -30,11 +46,13 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.texto}>Lista de Cursos</Text>
-        <SafeAreaView style={styles.conteudo}>
+        <SafeAreaView>
           <FlatList
             keyExtractor={(_, index) => index.toString()}
             data={this.state.data}
-            renderItem={({item, index}) => {
+            refreshing={this.state.isRefreshing}
+            onRefresh={() => this._getLista()}
+            renderItem={({ item }) => {
               return (
                 <View style={styles.item}>
                   <Text style={styles.conteudo}>{item.codigo}</Text>
@@ -43,8 +61,8 @@ export default class App extends React.Component {
                   <Text style={styles.conteudo}>{item.preco}</Text>
                   <Text style={styles.conteudo}>{item.categoria}</Text>
                 </View>
-            )
-          }}/>
+              )
+            }} />
         </SafeAreaView>
       </View>
     );
@@ -60,25 +78,20 @@ const styles = StyleSheet.create({
     paddingTop: '15%'
   },
   conteudo: {
-    margin: 10,
-    borderWidth: 1,
-    backgroundColor: 'white',
-    fontSize: 20
+    color: "#333333"
   },
   texto: {
     alignItems: "center",
     fontSize: 30,
+    fontWeight: 'bold',
     margin: 20,
-    color: '#FF0000',
+    color: '#000000',
   },
   item: {
+    width: width * 0.9,
     alignItems: "center",
-    backgroundColor: "#dcda44",
-    flexGrow: 1,
+    backgroundColor: "#add8e6",
     margin: 4,
     padding: 20
-  },
-  conteudo: {
-    color: "#333333"
   }
 });
